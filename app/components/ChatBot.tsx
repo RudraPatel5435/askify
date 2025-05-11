777abbbda9ab914d3fd9f39e4e6cd37dfe400040
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-05-11 20:11:30 +0530

    Minor

--- app/components/ChatBot.tsx
+++ app/components/ChatBot.tsx
@@ -57,10 +57,10 @@ export default function ChatBot() {
             <div className='px-35 py-5'>
                 <div className='flex items-center justify-between'>
                     <div className='font-semibold text-2xl'>Study Assistant</div>
-                    <div className='flex items-center gap-3 border-2 border-[#4941DA] bg-[#5a53db] hover:bg-[#4941DA] px-4 py-2 rounded-lg text-white cursor-pointer'>
+                    <button onClick={() => setChatHistory([])} className='flex items-center gap-3 border-2 border-[#4941DA] bg-[#5a53db] hover:bg-[#4941DA] px-4 py-2 rounded-lg text-white cursor-pointer'>
                         <Trash size={20} />
                         <span>Clear Chat</span>
-                    </div>
+                    </button>
                 </div>
 
                 <form onSubmit={async (e) => {
@@ -83,7 +83,7 @@ export default function ChatBot() {
                     <div className='flex items-center justify-between gap-5 mt-3 select-none'>
                         <div className='w-1/3 flex'>
                             <input type='radio' name='modeOpt' id='normalOpt' value='normal' defaultChecked className='peer hidden' />
-                            <label htmlFor='normalOpt' className='peer-checked:text-[#4F45E4] peer-checked:scale-103 peer-checked:border-[#4F45E4] peer-checked:bg-[#EEF2FF] bg-white border-1 border-borderp px-4 py-3 rounded-lg w-full hover:scale-103 cursor-pointer'>
+                            <label htmlFor='normalOpt' className='transform transition-transform duration-200 peer-checked:text-[#4F45E4] peer-checked:scale-103 peer-checked:border-[#4F45E4] peer-checked:bg-[#EEF2FF] bg-white border-1 border-borderp px-4 py-3 rounded-lg w-full hover:scale-103 cursor-pointer'>
                                 <div className='flex items-center justify-start gap-3'>
                                     <div className='p-2 bg-white rounded-full'><Bot /></div>
                                     <div className='font-medium'>Normal (Default)</div>
@@ -93,7 +93,7 @@ export default function ChatBot() {
                         </div>
                         <div className='w-1/3 flex'>
                             <input type='radio' name='modeOpt' id='momOpt' value='mom' className='peer hidden' />
-                            <label htmlFor='momOpt' className='peer-checked:text-[#4F45E4] peer-checked:scale-103 peer-checked:border-[#4F45E4] peer-checked:bg-[#EEF2FF] bg-white border-1 border-borderp px-4 py-3 rounded-lg w-full hover:scale-103 cursor-pointer'>
+                            <label htmlFor='momOpt' className='transform transition-transform duration-200 peer-checked:text-[#4F45E4] peer-checked:scale-103 peer-checked:border-[#4F45E4] peer-checked:bg-[#EEF2FF] bg-white border-1 border-borderp px-4 py-3 rounded-lg w-full hover:scale-103 cursor-pointer'>
                                 <div className='flex items-center justify-start gap-3'>
                                     <div className='p-2 bg-white rounded-full'><Angry /></div>
                                     <div className='font-medium'>Indian Mom (For Fun)</div>
@@ -103,7 +103,7 @@ export default function ChatBot() {
                         </div>
                         <div className='w-1/3 flex'>
                             <input type='radio' name='modeOpt' id='monkOpt' value='monk' className='peer hidden' />
-                            <label htmlFor='monkOpt' className='peer-checked:text-[#4F45E4] peer-checked:scale-103 peer-checked:border-[#4F45E4] peer-checked:bg-[#EEF2FF] bg-white border-2 border-borderp px-4 py-3 rounded-lg w-full hover:scale-103 cursor-pointer'>
+                            <label htmlFor='monkOpt' className='transform transition-transform duration-200 peer-checked:text-[#4F45E4] peer-checked:scale-103 peer-checked:border-[#4F45E4] peer-checked:bg-[#EEF2FF] bg-white border-1 border-borderp px-4 py-3 rounded-lg w-full hover:scale-103 cursor-pointer'>
                                 <div className='flex items-center justify-start gap-3'>
                                     <div className='p-2 bg-white rounded-full'><House /></div>
                                     <div className='font-medium'>Monk (For Fun)</div>
@@ -117,10 +117,6 @@ export default function ChatBot() {
                     <div className='messageDiv flex flex-col gap-3 bg-[#F3F4F6] h-140 w-full mt-5 mb-5 rounded-lg p-5 font-medium overflow-y-scroll'>
                         {
                             chatHistory.map((blob, idx) => (
-                                // <div key={idx} className={blob.role==='user'? 'bg-[#DFE7FF] px-4 py-2 rounded-xl w-fit self-end max-w-5/6': 'bg-white px-4 py-2 rounded-xl w-fit max-w-5/6'}>
-                                //     {blob.role==='user'?'You': blob.mode}<br />
-                                //     {blob.message}
-                                // </div>
                                 <div key={idx} className={blob.role === 'user' ? 'flex gap-3 items-center bg-[#DFE7FF] px-4 py-2 rounded-xl w-fit self-end max-w-5/6' : 'flex items-center gap-3 bg-white px-4 py-2 rounded-xl w-fit max-w-5/6'}>
                                     <div className={`p-2 rounded-full ${blob.role === 'user' ? 'hidden' : 'bg-[#E4E7EB]'}`}>
                                         {blob.role !== 'user' && blob.mode && emojis[blob.mode as keyof typeof emojis]}
@@ -131,6 +127,15 @@ export default function ChatBot() {
                         }
                         <div ref={messageRef} />
                     </div>
+
+                    {/* <div className='font-medium'
+                    Start a conversation
+                </div>art a conversation
+                <div className='text-sm text-zinc-200'>
+                <divAsk me anything about your engineering studies, upload documents, or get help with notes.
+                 /  Ask me anything about your engineering studies, upload documents, or get help with notes.
+                </
+                    </div> */}
 
 
                     <hr className='text-[#E4E7EB]' />
@@ -153,8 +158,9 @@ export default function ChatBot() {
                             <Send />
                         </button>
                     </div>
-                </form>
-            </div>
+                </form >
+            </div >
         </>
     )
 }
+
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,18 @@
-import Link from 'next/link'
 import React from 'react'
-export default function Navbar(){
-    return(
+import NavbarClient from './NavbarClient'
+export default function Navbar() {
+    return (
         <>
             <div className='flex items-center justify-between py-3 px-70 bg-white border-b-1 border-b-[#E4E7EB] text-lg'>
                 <div className='text-2xl text-[#4F45E4] font-semibold cursor-pointer'>
                     Askify
                 </div>
-                <div className='flex items-center justify-center gap-5 font-medium'>
-                    <Link href='/chat' className='hover:text-[#4F45E4] px-4 py-2 hover:bg-[#F3F4F6] rounded-md cursor-pointer'>Chat</Link>
-                    <Link href='/' className='hover:text-[#4F45E4] px-4 py-2 hover:bg-[#F3F4F6] rounded-md cursor-pointer'>Notes</Link>
-                </div>
+                <NavbarClient />
                 <div className='flex items-center justify-between gap-5'>
                     <div>Acc</div>
                     <div>Sign Out</div>
                 </div>
-            </div>             
+            </div>
         </>
     )
 }
